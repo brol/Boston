446e9ef172c338f56b171117fc2ccf34777d49d4
brol <brol31@gmail.com>
2015-05-05 18:31:30 +0200

    initial depot

---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Boston, a theme for Dotclear.
+#
+# Copyright (c) 2009
+# annso contact@as-i-am.fr
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+$this->registerModule(
+	/* Name */			"Boston",
+	/* Description*/	"Thème en tons de gris",
+	/* Author */		"annso, Pierre Van Glabeke",
+	/* Version */		'1.2',
+	array(
+		/* Type */		'type' =>			'theme',
+		              'dc_min' => '2.7'
+	)
+);
